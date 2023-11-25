109f3011e1fab3f85913de903ad723918feb4e3c
Angelo Rossi <angelo.rossi@epfl.ch>
2023-11-25 15:16:31 +0100

    clean up excess before merging with angelo

--- src/test.py
+++ src/test.py
@@ -12,40 +12,6 @@ validation, instead of using a testing-validating set.
 """
 
 @torch.no_grad()
-def crossValidate(model, device, train_loader, loss_fn):
-    model.eval()  # Important: eval mode (affects dropout, batch norm etc)
-    val_loss = 0
-    correct = 0
-    
-    # Define the sizes for training and testing sets
-    rest_size = int(0.8 * len(train_loader))
-    val_size = len(train_loader) - rest_size
-
-    # Split the training set
-    _, val_loader = random_split(train_loader, [rest_size, val_size])
-
-    for img_batch, gt_batch in val_loader:
-        img_batch, gt_batch = img_batch.to(device), gt_batch.to(device)
-        output = model(img_batch)
-        val_loss += loss_fn(output, gt_batch).item() * len(img_batch)
-        pred = output.argmax(
-            dim=1, keepdim=True
-        )  # get the index of the max log-probability
-        correct += pred.eq(gt_batch.view_as(pred)).sum().item()
-
-    val_loss /= len(val_loader.dataset)
-
-    print(
-        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)".format(
-            val_loss,
-            correct,
-            len(val_loader.dataset),
-            100.0 * correct / len(val_loader.dataset),
-        )
-    )
-    return val_loss, correct / len(val_loader.dataset)
-
-@torch.no_grad()
 def test_model(model, device, test_loader, loss_fn):
     model.eval()
     predictions = []
@@ -62,3 +28,37 @@ def test_model(model, device, test_loader, loss_fn):
     losses = loss_fn(predictions, gts)
 
     return losses, accuracies
+    
+# @torch.no_grad()
+# def crossValidate(model, device, train_loader, loss_fn):
+#     model.eval()  # Important: eval mode (affects dropout, batch norm etc)
+#     val_loss = 0
+#     correct = 0
+    
+#     # Define the sizes for training and testing sets
+#     rest_size = int(0.8 * len(train_loader))
+#     val_size = len(train_loader) - rest_size
+
+#     # Split the training set
+#     _, val_loader = random_split(train_loader, [rest_size, val_size])
+
+#     for img_batch, gt_batch in val_loader:
+#         img_batch, gt_batch = img_batch.to(device), gt_batch.to(device)
+#         output = model(img_batch)
+#         val_loss += loss_fn(output, gt_batch).item() * len(img_batch)
+#         pred = output.argmax(
+#             dim=1, keepdim=True
+#         )  # get the index of the max log-probability
+#         correct += pred.eq(gt_batch.view_as(pred)).sum().item()
+
+#     val_loss /= len(val_loader.dataset)
+
+#     print(
+#         "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)".format(
+#             val_loss,
+#             correct,
+#             len(val_loader.dataset),
+#             100.0 * correct / len(val_loader.dataset),
+#         )
+#     )
+#     return val_loss, correct / len(val_loader.dataset)
